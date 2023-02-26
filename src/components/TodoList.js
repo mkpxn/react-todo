e1380bceb0a35fc372a9f92bd52b94a1da04ec77
CBRA-Mirko <pixnermirko@gmail.com>
2023-02-26 16:16:40 +0100

    add further styling & count functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,24 +1,50 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Todo from './Todo';
 import './TodoList.css';
 
-const todos = [
+const allTodos = [
   {description: 'Einkaufen', done: false},
-  {description: 'Sport', done: true},
+  {description: 'Sport', done: false},
   {description: 'Programmieren', done: true},
 ];
 
 const TodoList = () => {
+
+  const [openCount, setCount] = useState(0);
+  const [todos, setTodos] = useState(allTodos);
+
+  const changeTodo = (index) => {
+    const newTodos = [...todos];
+    if (newTodos[index].done) {
+      newTodos[index].done = false;
+    } else {
+      newTodos[index].done = true;
+    }
+    setTodos(newTodos);
+  }
+
+  useEffect(() => {
+    const countOpen = () => {
+      const doneTodos = todos.filter((item) => {
+        return !item.done;
+      });
+      setCount(doneTodos.length);
+    };
+    countOpen();
+  }, [todos]);
+
   return (
-    <section className='todo-section'>
+    <section className='todo-list'>
       <header className='header'>
         <div className='header__inner'>
-          <h1>To-Dos</h1>
+          <h1>Hallo!</h1>
+          <p>Du hast noch <span className="header__opentodos">{openCount}</span> offene Todos</p>
         </div>
       </header>
       <ul className='todos'>
         {todos.map((item, index) => {
-          return <Todo description={item.description} done={item.done} key={index}/>
+          return <Todo description={item.description} done={item.done} key={index} index={index} onChangeTodo={changeTodo} />
         })}
       </ul>
     </section>
